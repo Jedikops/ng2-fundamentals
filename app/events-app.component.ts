0556a7a7fad0d750ca0a638905cf8bde0fcee0f5
jedikops <jedikops@gmail.com>
2018-02-05 19:44:16 +0100

    first commint module 3 done

---
+++ app/events-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+//import { EventsListComponent } from 'app'
+
+@Component({
+    selector: 'events-app',
+    template: `<nav-bar></nav-bar>
+    <events-list></events-list>`
+})
+export class EventsAppComponent{
+
+  
+}
